aacd308f0cceabfc1f0874b0f69e64ecf4fbe763
Palk1996 <palk19960@gmail.com>
2021-12-02 20:45:05 +0700

    doing responsive with main section

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,10 +1,19 @@
+import { useState } from 'react'
+import { AspectRatio } from 'react-aspect-ratio';
 import { Container, Row, Col } from 'react-bootstrap'
 import '../style/components/SideNav.css'
-import { FaHome, FaInfo, FaCertificate} from 'react-icons/fa'
-import { GiSkills} from 'react-icons/gi'
-import { AiFillProject} from 'react-icons/ai'
+import { FaHome, FaInfo, FaCertificate } from 'react-icons/fa'
+import { GiSkills } from 'react-icons/gi'
+import { AiFillProject } from 'react-icons/ai'
 
 function SideNav() {
+    const class_name = "toLightMode"
+    const [theme, setTheme] = useState(true)
+
+    const toggleDarkMode = () => {
+        setTheme(!theme)
+    }
+
     const navContent = [
         {
             title: "Home",
@@ -24,7 +33,7 @@ function SideNav() {
         {
             title: "Certificate",
             scrollTo: "/",
-            icon: <FaCertificate className="dot-icon"/>
+            icon: <FaCertificate className="dot-icon" />
         },
         {
             title: "Project",
@@ -35,20 +44,36 @@ function SideNav() {
     return (
         <div className="wrap-nav">
             <Container>
-                <Col className="nav-area">
+                <Col className={theme ? "nav-area theme-light" : "nav-area theme-dark"}>
                     <div className="nav">
                         <ul>
                             {
                                 navContent.map((value, index) => {
                                     return (
-                                        <Row>
-                                            <Col ><a className="dot-handler" key={index}>{value.icon}</a></Col>
-                                            <Col ><li className="nav-content" href={value.scrollTo}>{value.title}</li> </Col>
+                                        <Row className="hover-me" >
+                                                <Col ><a className="dot-handler" href={value.scrollTo}>{value.icon}</a></Col>
+                                                <Col ><li className="nav-content" key={index}>{value.title}</li> </Col>
                                         </Row>
                                     )
                                 })
                             }
                         </ul>
+                    </div>
+                    <div class="toggleWrapper">
+                        <input type="checkbox" onChange={toggleDarkMode} className="dn" id="dn" />
+                        <label for="dn" className="toggle">
+                            <span className="toggle__handler">
+                                <span className="crater crater--1"></span>
+                                <span className="crater crater--2"></span>
+                                <span className="crater crater--3"></span>
+                            </span>
+                            <span className="star star--1"></span>
+                            <span className="star star--2"></span>
+                            <span className="star star--3"></span>
+                            <span className="star star--4"></span>
+                            <span className="star star--5"></span>
+                            <span className="star star--6"></span>
+                        </label>
                     </div>
                 </Col>
             </Container>
